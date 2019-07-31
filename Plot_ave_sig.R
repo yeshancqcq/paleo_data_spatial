78d674b28ef20ae906e50d37be9f96779879aff7
Shan Ye <yeshan@umich.edu>
2019-07-30 19:05:03 -0500

    new anomaly calculation

--- Plot_ave_sig.R
+++ Plot_ave_sig.R
@@ -13,7 +13,7 @@ ave <- ggplot()+
   labs(y = "Anomaly (Celsius)",
        x = "Time (Years BP)",
        colour = "Legend") +
-  ggtitle("Global Avereage Temperature Anomaly Regarding 10000 Years BP, Area Weighted")+
+  ggtitle("Global Avereage Temperature Anomaly, Area Weighted","Anomaly reference: the average temperature from 8ka to 12 ka")+
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
         panel.background = element_blank(), axis.line = element_line(colour = "black"),
         axis.title.x=element_text(size=10),
@@ -22,7 +22,8 @@ ave <- ggplot()+
   scale_x_reverse(limits = c(22000, 0))+
   annotate("text", x = 20000, y = -5, label = "1-Sigma Uncertainty", size = 3 ,colour = "#457a96")+
   geom_hline(yintercept=0, linetype="dashed", color = "red")+
-  geom_vline(xintercept=10000, linetype="dashed", color = "red")
+  geom_vline(xintercept=12000, linetype="dashed", color = "red")+
+  geom_vline(xintercept=8000, linetype="dashed", color = "red")
   
 ave
   
