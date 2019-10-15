462ebe57ec1b422be996672bbcecf1a96a34cd9e
Shan Ye <yeshan@umich.edu>
2019-10-15 15:20:19 -0500

    Create diff_anomaly_raster.py

---
+++ diff_anomaly_raster.py
@@ -0,0 +1,31 @@
+import arcpy
+#from arcpy import env
+#from arcpy.sa import *
+arcpy.CheckOutExtension("spatial")
+arcpy.env.overwriteOutput = True
+
+arcpy.env.workspace = "C:/Users/Shan Ye/Documents/GitHub/paleo_data_spatial"
+polygon = "C:/Users/Shan Ye/Documents/GitHub/paleo_data_spatial/data/diff_anomaly.shp"
+
+allFields = arcpy.ListFields("C:/Users/Shan Ye/Documents/GitHub/paleo_data_spatial/data/diff_anomaly.shp", {"None"}, {"Double"})
+
+for field in allFields:
+
+    fieldName = str(field.name)
+
+    print ("Starting the processing " + fieldName)
+
+    if fieldName != "FID" and fieldName != "Shape" and fieldName != "PageName":
+
+        raster = str("C:/Users/Shan Ye/Documents/GitHub/paleo_data_spatial/diff_raster/" + fieldName)
+
+        # polygon to raster
+
+        arcpy.PolygonToRaster_conversion(polygon, fieldName, raster, 'CELL_CENTER', 'NONE', '5')
+
+        print (fieldName +" finished; ")
+
+       
+
+
+    
