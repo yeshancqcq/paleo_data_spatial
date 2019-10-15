12b655cbf2fe11f7366a8f5e58d9f4e987037478
Shan Ye <yeshan@umich.edu>
2019-10-15 15:20:21 -0500

    Create mapping_series.py

---
+++ mapping_series.py
@@ -0,0 +1,63 @@
+
+import arcpy  
+from arcpy import env  
+  
+# Set workspace  
+arcpy.env.workspace = "C:/Users/Shan Ye/Documents/GitHub/paleo_data_spatial/diff_raster"
+
+# Define the mxd file to use
+mxd = arcpy.mapping.MapDocument("C:/Users/Shan Ye/Documents/GitHub/paleo_data_spatial/paleo_process1.mxd")
+
+# Hook into the data frame where you want to add the layer  
+df  = arcpy.mapping.ListDataFrames(mxd)[0]  
+
+# Get list of raster datasets  
+rasterList = arcpy.ListRasters("*", "All")
+
+# Set the symbol layer
+symbolLyr = "C:/Users/Shan Ye/Documents/GitHub/paleo_data_spatial/symbol.lyr"
+
+# Loop through all rasters
+for raster in rasterList:
+
+    print ("Starting the processing " + raster)
+
+    #arcpy.ApplySymbologyFromLayer_management(raster, "symbol")
+    
+    # Set layer name of this raster
+    rasterName = str("this_layer")
+
+    # Create the raster layer
+    lyr = arcpy.MakeRasterLayer_management(raster, rasterName).getOutput(0)
+
+    # Add this layer to data frame
+    arcpy.mapping.AddLayer(df, lyr)
+
+    layerToUpdate = arcpy.mapping.ListLayers(mxd, "this_layer", df)[0] 
+
+    # Add title to the map
+    titleStr = str("Global Data-Model Temperature Anomaly Differences at " + raster +" (Years BP)")
+    titleItem = arcpy.mapping.ListLayoutElements(mxd, "TEXT_ELEMENT", "title")[0]
+    titleItem.text = titleStr 
+
+    # Apply the symbology of the "symbol" layer to this layer
+    arcpy.ApplySymbologyFromLayer_management(layerToUpdate, "C:/Users/Shan Ye/Documents/GitHub/paleo_data_spatial/symbol.lyr")
+
+    # Set the save directory of the map
+    mapstr = str("C:/Users/Shan Ye/Documents/GitHub/paleo_data_spatial/anomaly_diff_map/" + raster + ".jpg")
+
+    # Export map
+    arcpy.mapping.ExportToJPEG(mxd, mapstr)
+
+    # Remove this raster layer
+    arcpy.mapping.RemoveLayer(df, lyr)
+
+
+
+
+
+
+
+    
+
+    print ("finishing " + raster +";")
