f18420e8ea7e96db4aff88096469d7e45799ae77
yeshancqcq <yeshan@umich.edu>
2019-10-26 09:12:17 -0500

    new plots

--- plot_regional.R
+++ plot_regional.R
@@ -228,43 +228,6 @@ plot <- ggplot()+
 plot
 ggsave("../img/regional_plots/antarctica.jpg", width = 6, height = 4)
 
-#china
-plot_data <- data.frame(time=proxy$time, proxy=proxy$china, model=model$china, diff=diff$China)
-plot <- ggplot()+
-  geom_line(data=plot_data,aes(time, proxy, colour = "Anomaly in Proxy Data"))+
-  geom_line(data=plot_data,aes(time, model, colour = "Anomaly in Model Data"))+
-  geom_line(data=plot_data,aes(time, diff+20, colour = "Model - Proxy"))+
-  scale_colour_manual(values = c(
-    'Anomaly in Proxy Data' = 'blue',
-    'Anomaly in Model Data' = 'red',
-    'Model - Proxy' = 'purple'
-  ))+  
-  geom_hline(yintercept = 0, size = 0.6, linetype='dotted') +
-  geom_hline(yintercept = 20, size = 0.6, linetype='dotted', colour="purple") +
-  scale_y_continuous(name = expression("Temperature Anomaly (°C)"), limits = c(-25, 40), 
-                     sec.axis = sec_axis(~.-20, name = "Anomaly Differences (°C)"))+
-  labs(y = "Anomaly",
-       x = "Time (Years BP)",
-       colour = 'Legend') +
-  ggtitle("Temperature Anomaly at North-Central China", "Proxy Metadata and Model Outputs") +
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-        panel.background = element_blank(), axis.line.x = element_line(colour = "black"),
-        axis.title.y=element_text(size=10),
-        panel.border = element_rect(colour = "black", fill=NA),
-        axis.line= element_line(colour = "black"),
-        axis.title.x=element_text(size=10),
-        legend.justification = c(0, 0),
-        legend.position = "top",
-        legend.spacing.x = unit(10, 'pt'),
-        legend.text = element_text(margin = margin(t = 3), size=rel(0.83)),
-        legend.background = element_rect(colour = NA),
-        legend.key = element_rect(colour = "white", fill = NA),
-        legend.title = element_blank()
-  ) +
-  scale_x_reverse(limits = c(22000, 100), breaks = scales::pretty_breaks(n = 10))
-plot
-ggsave("../img/regional_plots/indonesia.jpg", width = 6, height = 4)
-
 #japan
 plot_data <- data.frame(time=proxy$time, proxy=proxy$japan, model=model$japan, diff=diff$Japan)
 plot <- ggplot()+
@@ -486,3 +449,336 @@ plot <- ggplot()+
   scale_x_reverse(limits = c(22000, 100), breaks = scales::pretty_breaks(n = 10))
 plot
 ggsave("../img/regional_plots/caribbean.jpg", width = 6, height = 4)
+
+# west coast of south america
+plot_data <- data.frame(time=proxy$time, proxy=proxy$s_america, model=model$s_america, diff=diff$W_S_America)
+plot <- ggplot()+
+  geom_line(data=plot_data,aes(time, proxy, colour = "Anomaly in Proxy Data"))+
+  geom_line(data=plot_data,aes(time, model, colour = "Anomaly in Model Data"))+
+  geom_line(data=plot_data,aes(time, diff+20, colour = "Model - Proxy"))+
+  scale_colour_manual(values = c(
+    'Anomaly in Proxy Data' = 'blue',
+    'Anomaly in Model Data' = 'red',
+    'Model - Proxy' = 'purple'
+  ))+  
+  geom_hline(yintercept = 0, size = 0.6, linetype='dotted') +
+  geom_hline(yintercept = 20, size = 0.6, linetype='dotted', colour="purple") +
+  scale_y_continuous(name = expression("Temperature Anomaly (°C)"), limits = c(-25, 40), 
+                     sec.axis = sec_axis(~.-20, name = "Anomaly Differences (°C)"))+
+  labs(y = "Anomaly",
+       x = "Time (Years BP)",
+       colour = 'Legend') +
+  ggtitle("Temperature Anomaly at West Coast of South America", "Proxy Metadata and Model Outputs") +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line.x = element_line(colour = "black"),
+        axis.title.y=element_text(size=10),
+        panel.border = element_rect(colour = "black", fill=NA),
+        axis.line= element_line(colour = "black"),
+        axis.title.x=element_text(size=10),
+        legend.justification = c(0, 0),
+        legend.position = "top",
+        legend.spacing.x = unit(10, 'pt'),
+        legend.text = element_text(margin = margin(t = 3), size=rel(0.83)),
+        legend.background = element_rect(colour = NA),
+        legend.key = element_rect(colour = "white", fill = NA),
+        legend.title = element_blank()
+  ) +
+  scale_x_reverse(limits = c(22000, 100), breaks = scales::pretty_breaks(n = 10))
+plot
+ggsave("../img/regional_plots/w_s_america.jpg", width = 6, height = 4)
+
+#congo
+plot_data <- data.frame(time=proxy$time, proxy=proxy$congo, model=model$congo, diff=diff$C_Africa)
+plot <- ggplot()+
+  geom_line(data=plot_data,aes(time, proxy, colour = "Anomaly in Proxy Data"))+
+  geom_line(data=plot_data,aes(time, model, colour = "Anomaly in Model Data"))+
+  geom_line(data=plot_data,aes(time, diff+20, colour = "Model - Proxy"))+
+  scale_colour_manual(values = c(
+    'Anomaly in Proxy Data' = 'blue',
+    'Anomaly in Model Data' = 'red',
+    'Model - Proxy' = 'purple'
+  ))+  
+  geom_hline(yintercept = 0, size = 0.6, linetype='dotted') +
+  geom_hline(yintercept = 20, size = 0.6, linetype='dotted', colour="purple") +
+  scale_y_continuous(name = expression("Temperature Anomaly (°C)"), limits = c(-25, 40), 
+                     sec.axis = sec_axis(~.-20, name = "Anomaly Differences (°C)"))+
+  labs(y = "Anomaly",
+       x = "Time (Years BP)",
+       colour = 'Legend') +
+  ggtitle("Temperature Anomaly at South Central Africa", "Proxy Metadata and Model Outputs") +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line.x = element_line(colour = "black"),
+        axis.title.y=element_text(size=10),
+        panel.border = element_rect(colour = "black", fill=NA),
+        axis.line= element_line(colour = "black"),
+        axis.title.x=element_text(size=10),
+        legend.justification = c(0, 0),
+        legend.position = "top",
+        legend.spacing.x = unit(10, 'pt'),
+        legend.text = element_text(margin = margin(t = 3), size=rel(0.83)),
+        legend.background = element_rect(colour = NA),
+        legend.key = element_rect(colour = "white", fill = NA),
+        legend.title = element_blank()
+  ) +
+  scale_x_reverse(limits = c(22000, 100), breaks = scales::pretty_breaks(n = 10))
+plot
+ggsave("../img/regional_plots/congo.jpg", width = 6, height = 4)
+
+#sw africa
+plot_data <- data.frame(time=proxy$time, proxy=proxy$w_africa, model=model$w_africa, diff=diff$W_Africa_coast)
+plot <- ggplot()+
+  geom_line(data=plot_data,aes(time, proxy, colour = "Anomaly in Proxy Data"))+
+  geom_line(data=plot_data,aes(time, model, colour = "Anomaly in Model Data"))+
+  geom_line(data=plot_data,aes(time, diff+20, colour = "Model - Proxy"))+
+  scale_colour_manual(values = c(
+    'Anomaly in Proxy Data' = 'blue',
+    'Anomaly in Model Data' = 'red',
+    'Model - Proxy' = 'purple'
+  ))+  
+  geom_hline(yintercept = 0, size = 0.6, linetype='dotted') +
+  geom_hline(yintercept = 20, size = 0.6, linetype='dotted', colour="purple") +
+  scale_y_continuous(name = expression("Temperature Anomaly (°C)"), limits = c(-25, 40), 
+                     sec.axis = sec_axis(~.-20, name = "Anomaly Differences (°C)"))+
+  labs(y = "Anomaly",
+       x = "Time (Years BP)",
+       colour = 'Legend') +
+  ggtitle("Temperature Anomaly at Tropical Southwestern African Coast", "Proxy Metadata and Model Outputs") +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line.x = element_line(colour = "black"),
+        axis.title.y=element_text(size=10),
+        panel.border = element_rect(colour = "black", fill=NA),
+        axis.line= element_line(colour = "black"),
+        axis.title.x=element_text(size=10),
+        legend.justification = c(0, 0),
+        legend.position = "top",
+        legend.spacing.x = unit(10, 'pt'),
+        legend.text = element_text(margin = margin(t = 3), size=rel(0.83)),
+        legend.background = element_rect(colour = NA),
+        legend.key = element_rect(colour = "white", fill = NA),
+        legend.title = element_blank()
+  ) +
+  scale_x_reverse(limits = c(22000, 100), breaks = scales::pretty_breaks(n = 10))
+plot
+ggsave("../img/regional_plots/sw_africa.jpg", width = 6, height = 4)
+
+#arabian sea
+plot_data <- data.frame(time=proxy$time, proxy=proxy$arabean_sea, model=model$arabean_sea, diff=diff$Arabian_sea)
+plot <- ggplot()+
+  geom_line(data=plot_data,aes(time, proxy, colour = "Anomaly in Proxy Data"))+
+  geom_line(data=plot_data,aes(time, model, colour = "Anomaly in Model Data"))+
+  geom_line(data=plot_data,aes(time, diff+20, colour = "Model - Proxy"))+
+  scale_colour_manual(values = c(
+    'Anomaly in Proxy Data' = 'blue',
+    'Anomaly in Model Data' = 'red',
+    'Model - Proxy' = 'purple'
+  ))+  
+  geom_hline(yintercept = 0, size = 0.6, linetype='dotted') +
+  geom_hline(yintercept = 20, size = 0.6, linetype='dotted', colour="purple") +
+  scale_y_continuous(name = expression("Temperature Anomaly (°C)"), limits = c(-25, 40), 
+                     sec.axis = sec_axis(~.-20, name = "Anomaly Differences (°C)"))+
+  labs(y = "Anomaly",
+       x = "Time (Years BP)",
+       colour = 'Legend') +
+  ggtitle("Temperature Anomaly at Arabean Sea", "Proxy Metadata and Model Outputs") +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line.x = element_line(colour = "black"),
+        axis.title.y=element_text(size=10),
+        panel.border = element_rect(colour = "black", fill=NA),
+        axis.line= element_line(colour = "black"),
+        axis.title.x=element_text(size=10),
+        legend.justification = c(0, 0),
+        legend.position = "top",
+        legend.spacing.x = unit(10, 'pt'),
+        legend.text = element_text(margin = margin(t = 3), size=rel(0.83)),
+        legend.background = element_rect(colour = NA),
+        legend.key = element_rect(colour = "white", fill = NA),
+        legend.title = element_blank()
+  ) +
+  scale_x_reverse(limits = c(22000, 100), breaks = scales::pretty_breaks(n = 10))
+plot
+ggsave("../img/regional_plots/arabean_sea.jpg", width = 6, height = 4)
+
+#iceland
+plot_data <- data.frame(time=proxy$time, proxy=proxy$iceland, model=model$iceland, diff=diff$Iceland)
+plot <- ggplot()+
+  geom_line(data=plot_data,aes(time, proxy, colour = "Anomaly in Proxy Data"))+
+  geom_line(data=plot_data,aes(time, model, colour = "Anomaly in Model Data"))+
+  geom_line(data=plot_data,aes(time, diff+20, colour = "Model - Proxy"))+
+  scale_colour_manual(values = c(
+    'Anomaly in Proxy Data' = 'blue',
+    'Anomaly in Model Data' = 'red',
+    'Model - Proxy' = 'purple'
+  ))+  
+  geom_hline(yintercept = 0, size = 0.6, linetype='dotted') +
+  geom_hline(yintercept = 20, size = 0.6, linetype='dotted', colour="purple") +
+  scale_y_continuous(name = expression("Temperature Anomaly (°C)"), limits = c(-25, 40), 
+                     sec.axis = sec_axis(~.-20, name = "Anomaly Differences (°C)"))+
+  labs(y = "Anomaly",
+       x = "Time (Years BP)",
+       colour = 'Legend') +
+  ggtitle("Temperature Anomaly at Iceland and British Isles", "Proxy Metadata and Model Outputs") +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line.x = element_line(colour = "black"),
+        axis.title.y=element_text(size=10),
+        panel.border = element_rect(colour = "black", fill=NA),
+        axis.line= element_line(colour = "black"),
+        axis.title.x=element_text(size=10),
+        legend.justification = c(0, 0),
+        legend.position = "top",
+        legend.spacing.x = unit(10, 'pt'),
+        legend.text = element_text(margin = margin(t = 3), size=rel(0.83)),
+        legend.background = element_rect(colour = NA),
+        legend.key = element_rect(colour = "white", fill = NA),
+        legend.title = element_blank()
+  ) +
+  scale_x_reverse(limits = c(22000, 100), breaks = scales::pretty_breaks(n = 10))
+plot
+ggsave("../img/regional_plots/iceland.jpg", width = 6, height = 4)
+
+#scandinavia
+plot_data <- data.frame(time=proxy$time, proxy=proxy$scandinavia, model=model$scandinavia, diff=diff$Scandinavia)
+plot <- ggplot()+
+  geom_line(data=plot_data,aes(time, proxy, colour = "Anomaly in Proxy Data"))+
+  geom_line(data=plot_data,aes(time, model, colour = "Anomaly in Model Data"))+
+  geom_line(data=plot_data,aes(time, diff+20, colour = "Model - Proxy"))+
+  scale_colour_manual(values = c(
+    'Anomaly in Proxy Data' = 'blue',
+    'Anomaly in Model Data' = 'red',
+    'Model - Proxy' = 'purple'
+  ))+  
+  geom_hline(yintercept = 0, size = 0.6, linetype='dotted') +
+  geom_hline(yintercept = 20, size = 0.6, linetype='dotted', colour="purple") +
+  scale_y_continuous(name = expression("Temperature Anomaly (°C)"), limits = c(-25, 40), 
+                     sec.axis = sec_axis(~.-20, name = "Anomaly Differences (°C)"))+
+  labs(y = "Anomaly",
+       x = "Time (Years BP)",
+       colour = 'Legend') +
+  ggtitle("Temperature Anomaly at Scandinavia, Svalbard and Denmark", "Proxy Metadata and Model Outputs") +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line.x = element_line(colour = "black"),
+        axis.title.y=element_text(size=10),
+        panel.border = element_rect(colour = "black", fill=NA),
+        axis.line= element_line(colour = "black"),
+        axis.title.x=element_text(size=10),
+        legend.justification = c(0, 0),
+        legend.position = "top",
+        legend.spacing.x = unit(10, 'pt'),
+        legend.text = element_text(margin = margin(t = 3), size=rel(0.83)),
+        legend.background = element_rect(colour = NA),
+        legend.key = element_rect(colour = "white", fill = NA),
+        legend.title = element_blank()
+  ) +
+  scale_x_reverse(limits = c(22000, 100), breaks = scales::pretty_breaks(n = 10))
+plot
+ggsave("../img/regional_plots/scandinavia.jpg", width = 6, height = 4)
+
+#Mediterranean
+plot_data <- data.frame(time=proxy$time, proxy=proxy$mediterranean, model=model$mediterranean, diff=diff$Mediterranean)
+plot <- ggplot()+
+  geom_line(data=plot_data,aes(time, proxy, colour = "Anomaly in Proxy Data"))+
+  geom_line(data=plot_data,aes(time, model, colour = "Anomaly in Model Data"))+
+  geom_line(data=plot_data,aes(time, diff+20, colour = "Model - Proxy"))+
+  scale_colour_manual(values = c(
+    'Anomaly in Proxy Data' = 'blue',
+    'Anomaly in Model Data' = 'red',
+    'Model - Proxy' = 'purple'
+  ))+  
+  geom_hline(yintercept = 0, size = 0.6, linetype='dotted') +
+  geom_hline(yintercept = 20, size = 0.6, linetype='dotted', colour="purple") +
+  scale_y_continuous(name = expression("Temperature Anomaly (°C)"), limits = c(-25, 40), 
+                     sec.axis = sec_axis(~.-20, name = "Anomaly Differences (°C)"))+
+  labs(y = "Anomaly",
+       x = "Time (Years BP)",
+       colour = 'Legend') +
+  ggtitle("Temperature Anomaly at Mediterranean Sea", "Proxy Metadata and Model Outputs") +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line.x = element_line(colour = "black"),
+        axis.title.y=element_text(size=10),
+        panel.border = element_rect(colour = "black", fill=NA),
+        axis.line= element_line(colour = "black"),
+        axis.title.x=element_text(size=10),
+        legend.justification = c(0, 0),
+        legend.position = "top",
+        legend.spacing.x = unit(10, 'pt'),
+        legend.text = element_text(margin = margin(t = 3), size=rel(0.83)),
+        legend.background = element_rect(colour = NA),
+        legend.key = element_rect(colour = "white", fill = NA),
+        legend.title = element_blank()
+  ) +
+  scale_x_reverse(limits = c(22000, 100), breaks = scales::pretty_breaks(n = 10))
+plot
+ggsave("../img/regional_plots/mediterranean.jpg", width = 6, height = 4)
+
+#south atlantic
+plot_data <- data.frame(time=proxy$time, proxy=proxy$s_atlantic, model=model$s_atlantic, diff=diff$South_Atlantic)
+plot <- ggplot()+
+  geom_line(data=plot_data,aes(time, proxy, colour = "Anomaly in Proxy Data"))+
+  geom_line(data=plot_data,aes(time, model, colour = "Anomaly in Model Data"))+
+  geom_line(data=plot_data,aes(time, diff+20, colour = "Model - Proxy"))+
+  scale_colour_manual(values = c(
+    'Anomaly in Proxy Data' = 'blue',
+    'Anomaly in Model Data' = 'red',
+    'Model - Proxy' = 'purple'
+  ))+  
+  geom_hline(yintercept = 0, size = 0.6, linetype='dotted') +
+  geom_hline(yintercept = 20, size = 0.6, linetype='dotted', colour="purple") +
+  scale_y_continuous(name = expression("Temperature Anomaly (°C)"), limits = c(-25, 40), 
+                     sec.axis = sec_axis(~.-20, name = "Anomaly Differences (°C)"))+
+  labs(y = "Anomaly",
+       x = "Time (Years BP)",
+       colour = 'Legend') +
+  ggtitle("Temperature Anomaly at South Atlantic", "Proxy Metadata and Model Outputs") +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line.x = element_line(colour = "black"),
+        axis.title.y=element_text(size=10),
+        panel.border = element_rect(colour = "black", fill=NA),
+        axis.line= element_line(colour = "black"),
+        axis.title.x=element_text(size=10),
+        legend.justification = c(0, 0),
+        legend.position = "top",
+        legend.spacing.x = unit(10, 'pt'),
+        legend.text = element_text(margin = margin(t = 3), size=rel(0.83)),
+        legend.background = element_rect(colour = NA),
+        legend.key = element_rect(colour = "white", fill = NA),
+        legend.title = element_blank()
+  ) +
+  scale_x_reverse(limits = c(22000, 100), breaks = scales::pretty_breaks(n = 10))
+plot
+ggsave("../img/regional_plots/south_atlantic.jpg", width = 6, height = 4)
+
+#china
+plot_data <- data.frame(time=proxy$time, proxy=proxy$china, model=model$china, diff=diff$China)
+plot <- ggplot()+
+  geom_line(data=plot_data,aes(time, proxy, colour = "Anomaly in Proxy Data"))+
+  geom_line(data=plot_data,aes(time, model, colour = "Anomaly in Model Data"))+
+  geom_line(data=plot_data,aes(time, diff+20, colour = "Model - Proxy"))+
+  scale_colour_manual(values = c(
+    'Anomaly in Proxy Data' = 'blue',
+    'Anomaly in Model Data' = 'red',
+    'Model - Proxy' = 'purple'
+  ))+  
+  geom_hline(yintercept = 0, size = 0.6, linetype='dotted') +
+  geom_hline(yintercept = 20, size = 0.6, linetype='dotted', colour="purple") +
+  scale_y_continuous(name = expression("Temperature Anomaly (°C)"), limits = c(-25, 40), 
+                     sec.axis = sec_axis(~.-20, name = "Anomaly Differences (°C)"))+
+  labs(y = "Anomaly",
+       x = "Time (Years BP)",
+       colour = 'Legend') +
+  ggtitle("Temperature Anomaly at North-Central China", "Proxy Metadata and Model Outputs") +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line.x = element_line(colour = "black"),
+        axis.title.y=element_text(size=10),
+        panel.border = element_rect(colour = "black", fill=NA),
+        axis.line= element_line(colour = "black"),
+        axis.title.x=element_text(size=10),
+        legend.justification = c(0, 0),
+        legend.position = "top",
+        legend.spacing.x = unit(10, 'pt'),
+        legend.text = element_text(margin = margin(t = 3), size=rel(0.83)),
+        legend.background = element_rect(colour = NA),
+        legend.key = element_rect(colour = "white", fill = NA),
+        legend.title = element_blank()
+  ) +
+  scale_x_reverse(limits = c(22000, 100), breaks = scales::pretty_breaks(n = 10))
+plot
+ggsave("../img/regional_plots/china.jpg", width = 6, height = 4)
