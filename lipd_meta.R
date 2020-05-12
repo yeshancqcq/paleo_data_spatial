fb7d589519f82c45e046050adde7cef2c4e6a4aa
yeshancqcq <yeshan@umich.edu>
2020-05-12 15:32:33 -0500

    Update lipd_meta.R

--- lipd_meta.R
+++ lipd_meta.R
@@ -1,11 +1,7 @@
-x = paste("~/Downloads/lipd/all/",filenames[1],sep="")
+x = paste("~/Downloads/lipd/lipd_data/",filenames[19],sep="")
 D = readLipd(path = toString(x))
 
-setwd("~/Documents/github/paleo_data_spatial")
-
 library(lipdR)
-
-setwd("~/Downloads/lipd/all")
 
 filenames <- list.files(path = "~/Downloads/lipd/lipd_data", pattern=NULL, all.files=FALSE,
                         full.names=FALSE)
@@ -21,7 +17,14 @@ metadata = data.frame(id = 1:length(filenames),
                       author = NA,
                       pub_year = NA,
                       title = NA,
-                      journal = NA)
+                      journal = NA,
+                      archive_type = NA,
+                      min_age = NA,
+                      max_age = NA,
+                      proxy = NA,
+                      num_record = NA,
+                      num_control = NA,
+                      url = NA)
 
 for(i in 1:length(filenames)){
   x = paste("~/Downloads/lipd/lipd_data/",filenames[i],sep="")
@@ -49,6 +52,40 @@ for(i in 1:length(filenames)){
   if(length(D[["pub"]][[1]][["journal"]])>0){
     metadata$journal[i] = D[["pub"]][[1]][["journal"]]
   }
-  cat("finishing", i, "of 455","\n")
+  if(length(D[["paleoData"]][[1]][["measurementTable"]][[1]][["temperature"]][["proxy"]])>0){
+    metadata$proxy[i] = D[["paleoData"]][[1]][["measurementTable"]][[1]][["temperature"]][["proxy"]]
+  }else if(length(D[["paleoData"]][[1]][["measurementTable"]])>=2){
+    if(!(is.null(D[["paleoData"]][[1]][["measurementTable"]][[1]][["temperature"]][["proxy"]]))){
+      metadata$proxy[i] = D[["paleoData"]][[1]][["measurementTable"]][[2]][["temperature"]][["proxy"]]
+    }
+  }else{
+    metadata$proxy[i] = NA
+  }
+  if(length(D[["archiveType"]])>0){
+    metadata$archive_type[i] = D[["archiveType"]]
+  }
+  if(!(is.null(D[["originalDataUrl"]]))){
+    metadata$url[i] = D[["originalDataUrl"]]
+  }
+  if(length(D[["maxYear"]])>0){
+    metadata$max_age[i] = D[["maxYear"]]
+  }
+  if(length(D[["minYear"]])>0){
+    metadata$min_age[i] = D[["minYear"]]
+  }
+  if(!(is.null(D[["paleoData"]][[1]][["measurementTable"]][[1]][["temperature"]][["values"]]))){
+    metadata$num_record[i] = length(D[["paleoData"]][[1]][["measurementTable"]][[1]][["temperature"]][["values"]])
+  }else if(length(D[["paleoData"]][[1]][["measurementTable"]])>=2){
+    if(!(is.null(D[["paleoData"]][[1]][["measurementTable"]][[1]][["temperature"]][["values"]]))){
+      metadata$num_record[i] = length(D[["paleoData"]][[1]][["measurementTable"]][[2]][["temperature"]][["values"]])
+    }
+  }else{
+    metadata$num_record[i] = 0
+  }
+  if(!(is.null(D[["chronData"]][[1]][["measurementTable"]][[1]][["age_type"]]))){
+    metadata$num_control[i] = length(D[["chronData"]][[1]][["measurementTable"]][[1]][["age_type"]])
+  }
+  cat("finishing", i, "of 455 or so","\n")
 }
-write.csv(metadata, file="~/Documents/github/paleo_data_spatial/data/lipd_metadata.csv",fileEncoding="UTF-8")
+write.csv(metadata, file="~/Documents/github/paleo_data_spatial/data/lipd_metadata_1.csv",fileEncoding="UTF-8")
+
